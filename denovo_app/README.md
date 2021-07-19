644c9d9ff1060d4149bb0fe37da50ae0104e64c0
castrocp <castrocp@umich.edu>
2021-07-19 14:10:59 -0400

    practicing with shiny features

--- denovo_app/README.md
+++ denovo_app/README.md
@@ -1,5 +1,5 @@
 A Shiny app is contained in a single script called `app.R`. The script lives in a directory and can be run with `runApp("directory_name")`.  
 `app.R` has three components:  
-- a user interface (ui) object  
+- a user interface (`ui`) object  
 - a server function  
--a call to the `shinyApp` function  
+- a call to the `shinyApp` function  
--- denovo_app/app.R
+++ denovo_app/app.R
@@ -1,1 +1,43 @@
 library(shiny)
+
+# Define UI -----------
+ui <- fluidPage( 
+  titlePanel("A Shiny app for viewing de novo mutations"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      h2("This is the sidebar panel"),
+      p("I can put something here that allows the user to perform statistical tests"),
+    
+    helpText("Create demographic maps with 
+               information from the 2010 US Census."),
+    
+    selectInput("var", 
+                label = "Choose a variable to display",
+                choices = list("Percent White", 
+                               "Percent Black",
+                               "Percent Hispanic", 
+                               "Percent Asian"),
+                selected = "Percent White"),
+    
+    sliderInput("range", 
+                label = "Range of interest:",
+                min = 0, max = 100, value = c(0, 100))
+    ),
+  
+    mainPanel(
+      h1("This is where I can put the data table containing the list of mutations"),
+      p("The table will be sortable by the different mutation attributes")
+    )
+  )
+) #fluidPage creates a display that automatically adjusts to the dimensions of the user's browser window.
+
+
+# Define server logic -----------
+server <- function(input, output) {
+  
+}
+
+
+# Run the app -----------
+shinyApp(ui = ui, server = server)
